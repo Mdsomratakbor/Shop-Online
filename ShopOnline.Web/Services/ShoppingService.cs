17788fa182b674d426166652f08fc5d53519f334
MdSomratAkbor <somrat876@gmail.com>
2023-09-11 18:57:16 +0600

    shoping cart method implement

---
+++ ShopOnline.Web/Services/ShoppingService.cs
@@ -0,0 +1,45 @@
+﻿using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services
+{
+    public class ShoppingService : IShoppingService
+    {
+        private readonly HttpClient httpClient;
+
+        public ShoppingService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<CartItemDto> AddItem(CartItemToAddDto cartItemToAddDto)
+        {
+            try
+            {
+                var response = await httpClient.PostAsJsonAsync<CartItemToAddDto>("api/ShoppingCart", cartItemToAddDto);
+                if (response.IsSuccessStatusCode)
+                {
+                    if(response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return default(CartItemDto);
+                    }
+                    return await response.Content.ReadFromJsonAsync<CartItemDto>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Http status:{response.StatusCode} Message -{message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception (ex.Message);
+            }
+        }
+
+        public Task<IEnumerable<CartItemDto>> GetItems(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
